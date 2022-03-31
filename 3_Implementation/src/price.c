f0d353c32faf1f33a26d686922c647f1eb3229da
Vishwas Tarende <vishwas007t@gmail.com>
2022-03-31 21:25:44 +0530

    Update price.c
    changes

--- 3_Implementation/src/price.c
+++ 3_Implementation/src/price.c
@@ -3,7 +3,7 @@ int change_Price_Ticket(int price)
 {
 	char passWord[10],pak[10]="admin";
 	printf("Enter the password to change price of ticket: ");
-	scanf("%s",&passWord);
+	scanf("%s",passWord);
 	if (strcmp(passWord,pak)==0)
 	{
 		printf("Please enter new price: ");
