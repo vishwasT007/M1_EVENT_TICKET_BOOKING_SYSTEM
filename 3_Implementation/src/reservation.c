f9fcb376a2278450e747cadd71d38ab493438c50
Vishwas Tarende <vishwas007t@gmail.com>
2022-04-02 16:39:36 +0530

    Update reservation.c

--- 3_Implementation/src/reservation.c
+++ 3_Implementation/src/reservation.c
@@ -3,46 +3,62 @@
 #include <string.h>
 
 struct event_details person[300];
+
 void reserve_Ticket(int *array, int price, int selection)
 {
 	int i, j;
+	
 	int count = 0;
+	
 	int id1 = 1000;
+	
 	printf("\n                                SCREEN\n\n\n");
 	for (i = 1; i <= 100; i++)
 	{
-		if (array[i] == 0)
+		if (array[i] == 0) {
 			printf("%d\t", i);
-		else
+		}
+		else {
 			printf("%d*\t", i);
-		if (i % 10 == 0)
+		}
+		if (i % 10 == 0) {
 			printf("\n\n");
+		}
 	}
-	printf("Please enter your name: ");
+	
+	printf("Please Enter Your Name: ");
 	scanf("%s",person[count].name);
-	printf("Please enter your phone number: ");
+	
+	printf("Please Enter Your Mobile Number: ");
 	scanf("%11s", person[count].mobile_number);
-	printf("Please select seat number you want? ");
+	
+	printf("Please Select Seat Number ? ");
 	scanf("%d", &j);
+	
 	if (j > 100 || j < 1)
 	{
-		printf("seat1 number is unavailable in this theater\n");
-		printf("Please re-enter seat number: ");
+		printf("Seat number is Not  Available\n");
+		printf("Please Enter Again seat number: ");
 		scanf("%d", &j);
 	}
+	
 	if (array[j] == 1)
 	{
-		printf("Sorry, this ticket is already booked! Please choose another seat.\n");
+		printf("Sorry !!, This ticket is already booked! Please choose another seat.\n");
 		scanf("%d", &j);
 	}
 	else
 		array[j] = 1;
 	person[count].seat_number = j;
-	if (selection == 1)
+	
+	if (selection == 1) {
 		event_show1(j, person[count].name, id1, price);
-	else if (selection == 2)
+	}
+	else if (selection == 2) {
 		event_show2(j, person[count].name, id1, price);
-	else
+	}
+	else {
 		event_show3(j, person[count].name, id1, price);
+	}
 	id1--;
 }
