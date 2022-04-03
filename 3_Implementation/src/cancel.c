0918d62e4b999311f03ea6343dd84235b7702d59
Vishwas Tarende <vishwas007t@gmail.com>
2022-04-03 18:28:32 +0530

    Update cancel.c

--- 3_Implementation/src/cancel.c
+++ 3_Implementation/src/cancel.c
@@ -3,22 +3,27 @@
 #include<string.h>
 
 struct event_details person[300];
+
 void cancel_ticket(int *array)
 {
-      int Choiceseat,i,hold;
+      	int Choiceseat;
+	int i;
+	int hold;
+	
 	  printf("Please enter ID number of ticket: ");
 	  scanf("%d",&Choiceseat);
-	  for (i=0;i<300;i++)
+	
+	  for (i = 0;i < 300;i++)
 	  {
-	  		if(Choiceseat==person[i].id)
+	  		if(Choiceseat == person[i].id)
 	  		{
-					 hold=5;
+					 hold = 5;
 					 system("cls");
 					 printf("%s your ticket is %d cancelled",person[i].name,person[i].seat_number);
-					 array[person[i].seat_number]=0;
-					 i=300;
+					 array[person[i].seat_number]= 0;
+					 i = 300;
 	  		}
 	  }
-	  if (hold!=5)
+	  if (hold != 5)
 	  		printf("Ticket ID number is incorrect please enter right one to cancel ticket: \n");
 }
